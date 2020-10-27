aa2f05e2adde91c1414288ad059d9f16510c1e35
Gianmarco <gianmarco.cavalieredev@gmail.com>
2020-10-27 17:19:35 +0100

    Esercizio finito con alternativa

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -1,13 +1,25 @@
 function reverseString(user_word) {
     user_word = user_word.toLowerCase()
-    var reversed_string = user_word.split('').reverse().join('');
+    /*var stringa_invertita = user_word.split('').reverse().join('');
 
-    if (user_word == reversed_string) {
+    if (user_word == stringa_invertita) {
         console.log("E Palindromo!")
     } else {
         console.log("Non e Palindromo!")
+    }*/
+    var message = '';
+    var str = "";
+    var stringa_invertita = user_word;
+    for (var reverse_index = stringa_invertita.length; reverse_index >= 0; reverse_index--) {
+        str += stringa_invertita.charAt(reverse_index);
     }
-    return reversedString
+    if (user_word == str) {
+        message = "Palindroma";
+    } else {
+        message = "Non e' Palindroma";
+    }
+    console.log(message);
+    return message;
 
 }
 
