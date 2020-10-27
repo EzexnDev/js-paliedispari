6691972bbacf0bb4062496cec7d68d50f968763e
Gianmarco <gianmarco.cavalieredev@gmail.com>
2020-10-27 20:52:04 +0100

    Fix

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -1,17 +1,19 @@
+
+/*var stringa_invertita = user_word.split('').reverse().join('');
+
+if (user_word == stringa_invertita) {
+    console.log("E Palindromo!")
+} else {
+    console.log("Non e Palindromo!")
+}*/
+
+var message = '';
+var str = "";
+
 function reverseString(user_word) {
     user_word = user_word.toLowerCase()
-    /*var stringa_invertita = user_word.split('').reverse().join('');
-
-    if (user_word == stringa_invertita) {
-        console.log("E Palindromo!")
-    } else {
-        console.log("Non e Palindromo!")
-    }*/
-    var message = '';
-    var str = "";
-    var stringa_invertita = user_word;
-    for (var reverse_index = stringa_invertita.length; reverse_index >= 0; reverse_index--) {
-        str += stringa_invertita.charAt(reverse_index);
+    for (var reverse_index = user_word.length; reverse_index >= 0; reverse_index--) {
+        str += user_word.charAt(reverse_index);
     }
     if (user_word == str) {
         message = "Palindroma";
@@ -20,7 +22,6 @@ function reverseString(user_word) {
     }
     console.log(message);
     return message;
-
 }
 
 reverseString(prompt('Inserisci la parola per verificare se e Palindroma'));
--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,31 +1,22 @@
-var message = ''
+var usr_choice = prompt("Scegli Pari o Dispari");
+var usr_number = prompt("Scegli un numero tra 1 e 5");
 
-function play(even_odd, user_numb) {
 
-    even_odd = even_odd.toLowerCase();
-    console.log(even_odd);
-    var random_numb = Math.floor(Math.random() * (5 - 1)) + 1;
-    console.log(random_numb);
-    var final_value = parseInt(random_numb) + parseInt(user_numb);
-    console.log(final_value);
-
-    if (even_odd == 'pari') {
-        if (final_value % 2 == 0) {
-            message = "Hai Vinto";
-        } else {
-            message = "Hai Perso";
-        }
-    } else if (even_odd == 'dispari') {
-        if (final_value % 2 == 1) {
-            message = "Hai Vinto";
-        } else {
-            message = "Hai Perso";
-        }
-    } else {
-        alert('Non hai inserito pari o dispari')
-    }
-    return message;
+function getRandom(min_numb,max_numb) {
+    var random_numb = Math.floor(Math.random() * (max_numb - min_numb)) + min_numb;
+    return random_numb
 }
 
-play(prompt("Pari o Dispari?"), prompt("Scegli un numero da 1 a 5"));
-console.log(message);
+
+function isEven(number){
+    return number%2==0;
+}
+
+var final_numb = parseInt(usr_number) + getRandom(1,5);
+console.log(final_numb);
+var result = isEven(final_numb);
+if((usr_choice =='pari' && result) || (usr_choice =='dispari' && !result)){
+    console.log("Hai vinto")
+} else {
+    console.log("Hai perso")
+}
